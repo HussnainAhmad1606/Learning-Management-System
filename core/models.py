ab72c3b1c48a9ba1e4c4b58a76b74fff3a05fbbf
HussnainAhmad1606 <hussnainahmad1606@gmail.com>
2021-08-01 16:48:30 +0500

    Create a new superuser, create 2 database tables, one for all courses, and second for single video in a course, add media directory and image show function on Django Admin Panel

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+	course_id = models.AutoField(primary_key= True)
+	course_name = models.CharField(max_length=70)
+	course_description = models.CharField(max_length=200)
+	course_photo = models.ImageField()
+	course_link = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.course_name
+
+
+class Video(models.Model):
+	video_id = models.AutoField(primary_key=True)
+	video_name = models.CharField(max_length=50)
+	video_description = models.CharField(max_length=200)
+	video_embed_code = models.CharField(max_length=300)
+	video_category = models.ManyToManyField(Course)
+
+	def __str__(self):
+		return f"{self.video_name}"
